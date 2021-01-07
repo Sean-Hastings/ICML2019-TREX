e82c70f91aeeae88c0d31f4c3f1bced218669270
Sean Hastings <sean_hastings@brown.edu>
2021-01-07 09:08:31 -0500

    reduced batch size and dataset size for faster training

--- atari/LearnAtariReturn.py
+++ atari/LearnAtariReturn.py
@@ -158,7 +158,7 @@ if __name__=="__main__":
     l1_reg = 0.00001
     stochastic = True
 
-    batch_size = 64
+    batch_size = 32
 
     env = make_vec_env(env_id, env_type, 1, seed,
                        wrapper_kwargs={
